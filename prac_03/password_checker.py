6c93faf3d63ba24786367ffbc0a9bc0e229bc9aa
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2018-08-14 13:09:47 +1000

    Refactor password program to use functions.

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -4,11 +4,20 @@ MIN_LENGTH = 5
 
 
 def main():
+    password = get_password()
+    print_hidden_length(password)
+
+
+def print_hidden_length(password):
+    for character in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
     password = input("Enter your password:")
     while len(password) < MIN_LENGTH:
         password = input("Enter a password {} characters long:".format(MIN_LENGTH))
-    for character in range(len(password)):
-        print("*", end="")
+    return password
 
 
 main()
