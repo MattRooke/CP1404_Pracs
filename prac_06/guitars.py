71b9781ca3d01180c2fcb64364fca656a5a729ab
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2018-09-04 16:30:23 +1000

    Add prac_06 tasks fix

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,8 +6,8 @@ while name:
     year = int(input("Year: "))
     cost = float(input("Cost: "))
     guitars.append(Guitar(name, year, cost))
+    print("{} ({}) : ${:.2f} added".format(name, year, cost))
     name = input("Name: ")
-    print(, "added")
 
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 
