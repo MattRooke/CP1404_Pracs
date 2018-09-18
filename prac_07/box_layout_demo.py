b0cbdb55c744a71477399f00ed9cae28c660556c
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2018-09-18 13:50:16 +1000

    Add prac_07 in class work

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,16 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("Greeted...")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+        self.root.ids.output_label.color = (1, 1, 1, 1)
+
+    def handle_clear(self):
+        print("Cleared...")
+        self.root.ids.output_label.text = "Enter your name"
+        self.root.ids.output_label.color = (1, 1, 0, 1)
+        self.root.ids.input_name.text = ""
+
 
 BoxLayoutDemo().run()
